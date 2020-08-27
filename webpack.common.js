d785b7aed0dc05eb7186ecfab95456a096ae7bee
alexandriebnk <alexandrie.be@gmail.com>
2020-08-27 15:12:43 +0200

    Section descr - WIP

---
+++ webpack.common.js
@@ -0,0 +1,37 @@
+const path = require("path");
+
+module.exports = {
+	entry:  "./src/js/index.js",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "js/bundle.js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.html$/,
+				use: ["html-loader"]
+			},
+			{
+				test: /\.svg|png|jpg|gif$/,
+				use: {
+					loader : "file-loader",
+					options : {
+						name: "[name].[ext]",
+						outputPath: "img"
+					}
+				}
+			},
+			{
+				test: /\.mp4|webm$/,
+				use: {
+					loader : "file-loader",
+					options : {
+						name: "[name].[ext]",
+						outputPath: "video"
+					}
+				}
+			}
+		]
+	}
+};
